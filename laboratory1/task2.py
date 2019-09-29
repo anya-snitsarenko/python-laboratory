c18c1be4d0c5db7c0800a077000b229bbe91d6ce
Ann <annasss187@gmail.com>
2019-09-30 00:29:37 +0300

    завдання 2

---
+++ laboratory1/task2.py
@@ -0,0 +1,6 @@
+print("Сніцаренко Анна Сергіївна \nКМ-91 \nЛабораторна робота №1 \nЗнайти max {min (a, b), min (c, d)} \n")
+a = float(input("Введіть значення a"))
+b = float(input("Введіть значення b"))
+c = float(input("Введіть значення c"))
+d = float(input("Введіть значення d"))
+print(max(min(a, b), min(c, d)))
